02c25bd6364ede3e9dfe8fc6cf42538318037fd6
nmx <nahu_m@hotmail.com>
2020-09-22 18:04:08 -0300

    cambios navbar inicio y index titulo

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -11,7 +11,7 @@ function Nav({ }) {
 
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <a className="navbar-brand" href="/">Henry Labs Nahuel</a>
+      <a className="navbar-brand" href="/">Inicio</a>
       <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
